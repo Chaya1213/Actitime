614a71b705f012c41b980fb860e2d5459cbd0bb2
chaya <chaya.satish13@gmail.com>
2021-03-15 13:01:52 +0530

    First Build

---
+++ com.combat/src/test/resources/ContactTest.java
@@ -0,0 +1,16 @@
+
+public class ContactTest {
+	@Test
+	public void createcustomer() {
+		System.out.println("CraeteCustomer");
+	}
+	@Test
+	public void deletecustomer() {
+		System.out.println("DeleteCustomer");
+	}
+	@Test
+	public void modifycustomer() {
+		System.out.println("ModifyCustomer");
+	}
+
+}

3a1cd9a677bf92e4c7f90621275614509fed6ac9
chaya <chaya.satish13@gmail.com>
2021-03-15 13:37:31 +0530

    Added CreateContact

---
+++ com.combat/src/main/java/com/combat/objrepo/CreateContacact.java
@@ -0,0 +1,11 @@
+package com.combat.objrepo;
+
+import org.testng.annotations.Test;
+
+public class CreateContacact {
+	@Test
+	public void createcontact() {
+		System.out.println("Createcontact");
+	}
+
+}
